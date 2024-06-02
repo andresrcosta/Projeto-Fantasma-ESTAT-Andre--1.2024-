5c2e87e1ff17da76fc42e251edccc462d3460a81
André Costa <147108697+andresrcosta@users.noreply.github.com>
2024-06-02 01:38:05 -0300

    Update código_completo_final.R

--- código_completo_final.R
+++ código_completo_final.R
@@ -344,72 +344,54 @@ print(teste_correlacao_de_correlacao_4)
 ### ANALISE 5 ###
 #################
 
-# Coluna indicando quem capturou o monstro
+# Criando um novo data frame para armazenar os resultados
 
-dados_analise_5 <- dados_analise_5 %>%
-  mutate(capturador = case_when(
-    caught_fred == TRUE ~ 'Fred',
-    caught_daphnie == TRUE ~ 'Daphne',
-    caught_velma == TRUE ~ 'Velma',
-    caught_shaggy == TRUE ~ 'Shaggy',
-    caught_scooby == TRUE ~ 'Scooby'
-  ))
+novo_dataset <- data.frame(Capturador = character(), Engajamento = numeric())
 
+# Função para extrair e processar os dados de cada coluna
+processar_coluna <- function(coluna_nome, nome_capturador) {
+  
+  # Filtrando os dados da coluna onde o valor é True
+  dados_filtrados <- filter(dados, !!sym(coluna_nome) == "True")
+  
+  # Adicionando os dados filtrados ao novo dataset
+  novo_dataset <<- bind_rows(novo_dataset, data.frame(Capturador = nome_capturador, Engajamento = dados_filtrados$engagement))
+}
 
-dados_analise_5 <- na.omit(dados_analise_5)
+# Listando das colunas de interesse e seus respectivos nomes de capturador formatados
 
-# Transformar as colunas de captura em fatores para facilitar a análise
-dados_analise_5 <- dados_analise_5 %>%
-  mutate(across(starts_with("caught_"), as.logical),
-         across(starts_with("captured_"), as.logical))
+colunas_interesse <- c("caught_fred", "caught_daphnie", "caught_velma", "caught_shaggy", "caught_scooby", "caught_other", "caught_not")
+nomes_capturadores <- c("Fred", "Daphnie", "Velma", "Salsicha", "Scooby", "Outro", "Não capturado")
 
+# Processando cada coluna
+for (i in 1:length(colunas_interesse)) {
+  processar_coluna(colunas_interesse[i], nomes_capturadores[i])
+}
 
-dados_analise_5 <- dados_analise_5 %>%
-  rowwise() %>%
-  mutate(character_caught = case_when(
-    caught_fred ~ "Fred",
-    caught_daphnie ~ "Daphne",
-    caught_velma ~ "Velma",
-    caught_shaggy ~ "Shaggy",
-    caught_scooby ~ "Scooby",
-    TRUE ~ "None"
-  )) %>%
-  ungroup()
+# Quadro de medidas
 
-dados_analise_5 <- dados_analise_5 %>%
-  filter (character_caught !="None")
+quadro_resumo_5 <- novo_dataset %>%
+  group_by( Capturador ) %>% 
+  summarize (Média = round(mean(Engajamento),2),
+             `Desvio Padrão ` = round(sd(Engajamento),2),
+             `Variância ` = round(var(Engajamento),2),
+             `Mínimo ` = round(min(Engajamento),2),
+             `1º Quartil ` = round(quantile(Engajamento , probs = .25),2),
+             Mediana = round(quantile(Engajamento , probs = .5),2),
+             `3º Quartil ` = round(quantile(Engajamento , probs = .75),2),
+             `Máximo ` = round(max(Engajamento),2)) %>% t() %>% as.data.frame ()
 
-ggplot(dados_analise_5) +
-  aes(x = reorder(character_caught, engagement, FUN = median), y = engagement) +
+xtable :: xtable(quadro_resumo_5)
+
+# Boxplot
+
+ggplot(novo_dataset) +
+  aes(x = reorder(Capturador, Engajamento, FUN = median), y = Engajamento) +
   geom_boxplot(fill = c("#A11D21"), width = 0.5) +
   stat_summary(
     fun = "mean", geom = "point", shape = 23, size = 3, fill = "white"
   ) +
-  labs(x = "Personagem que capturou o monstro", y = "Engajamento") +
+  labs(x = "Personagem que capturou o monstro", y = "Enagjamento do episódio") +
   theme_estat()
-ggsave("box_bi_analise_5.pdf", width = 158, height = 93, units = "mm")
+ggsave("box_bi_ultimo.pdf", width = 158, height = 93, units = "mm")
 
-
-quadro_resumo_5 <- dados_analise_5 %>%
-  group_by( character_caught ) %>% # caso mais de uma categoria
-  summarize (Média = round(mean(engagement),2),
-             `Desvio Padrão ` = round(sd(engagement),2),
-             `Variância ` = round(var(engagement),2),
-             `Mínimo ` = round(min(engagement),2),
-             `1º Quartil ` = round(quantile(engagement , probs = .25),2),
-             Mediana = round(quantile(engagement , probs = .5),2),
-             `3º Quartil ` = round(quantile(engagement , probs = .75),2),
-             `Máximo ` = round(max(engagement),2)) %>% t() %>% as.data.frame()
-xtable :: xtable(quadro_resumo_5)
-
-
-shapiro_test_5 <- shapiro.test(dados_analise_5$engagement)
-print(shapiro_test_5)
-
-
-resultado_bartlett_5 <- bartlett.test( engagement ~ character_caught, data = dados_analise_5)
-
-print(resultado_bartlett_5)
-
-anova_5 <- aov(engagement ~ character_caught, data = dados_analise_5)
-summary(anova_5)
